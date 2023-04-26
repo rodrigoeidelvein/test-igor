70c795de991723fa2eb9484ac72439575d455fff
Rodrigo Eidelvein <rodrigo.e@getjobber.com>
2023-04-26 16:06:25 -0300

    first commit

---
+++ index.test.js
@@ -0,0 +1,46 @@
+const paintBucket = (input, pixel, prevColor, newColor) => {
+  const row = pixel[0];
+  const col = pixel[1];
+
+  if (input[row][col] != prevColor) return;
+  const splitRow = input[row].split("");
+  splitRow[col] = newColor;
+
+  input[row] = splitRow.join("");
+
+  if (row > 0) {
+    paintBucket(input, [row - 1, col], prevColor, newColor);
+  }
+
+  if (row < input.length - 1) {
+    paintBucket(input, [row + 1, col], prevColor, newColor);
+  }
+
+  if (col > 0) {
+    paintBucket(input, [row, col - 1], prevColor, newColor);
+  }
+
+  if (col < input[0].length - 1) {
+    paintBucket(input, [row, col + 1], prevColor, newColor);
+  }
+
+  return input;
+};
+
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substring(0, index) +
+    replacement +
+    this.substring(index + replacement.length)
+  );
+};
+
+describe("PaintBucket", () => {
+  it("should paint the line", () => {
+    const input = [".###..", ".#..#.", ".###..", ".#...."];
+
+    const expectedOutput = [".OOO..", ".O..#.", ".OOO..", ".O...."];
+
+    expect(paintBucket(input, [0, 1], input[0][1], "O")).toStrictEqual(expectedOutput);
+  });
+});
